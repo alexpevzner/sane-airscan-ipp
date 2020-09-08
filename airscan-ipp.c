2564c4f4f53a2eafdb974a99354b364d8772bbcc
rishabharya <rishabharya2799@gmail.com>
2020-09-09 03:07:21 +0530

    Inital IPP SCAN setup

---
+++ airscan-ipp.c
@@ -0,0 +1,56 @@
+/* AirScan (a.k.a. IPP) backend for SANE
+ *
+ * Copyright (C) 2019 and up by Alexander Pevzner (pzz@apevzner.com)
+ * See LICENSE for license terms and conditions
+ *
+ * ESCL protocol handler
+ */
+#include "airscan.h"
+
+
+
+/* proto_handler_escl represents IPP protocol handler
+ */
+typedef struct {
+    proto_handler proto; /* Base class */
+} proto_handler_ipp;
+
+
+
+
+
+
+/* Free IPP protocol handler
+ */
+static void
+ipp_free (proto_handler *proto)
+{
+    mem_free(proto);
+}
+
+/* proto_handler_ipp_new creates new IPP protocol handler
+ */
+proto_handler*
+proto_handler_ipp_new (void)
+{
+    proto_handler_ipp *ipp = mem_new(proto_handler_ipp, 1);
+
+    ipp->proto.name = "IPP";
+    ipp->proto.free = ipp_free;
+
+    ipp->proto.devcaps_query = ipp_devcaps_query;
+    ipp->proto.devcaps_decode = ipp_devcaps_decode;
+
+    ipp->proto.scan_query = ipp_scan_query;
+    ipp->proto.scan_decode = ipp_scan_decode;
+
+    ipp->proto.load_query = ipp_load_query;
+    ipp->proto.load_decode = ipp_load_decode;
+
+    ipp->proto.status_query = ipp_status_query;
+    ipp->proto.status_decode = ipp_status_decode;
+
+    ipp->proto.cancel_query = ipp_cancel_query;
+
+    return &ipp->proto;
+}
